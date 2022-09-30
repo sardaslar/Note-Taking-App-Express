84b914e4ccf51eee8727ecc4bfa8d30d1366a7fc
Can Sardaslar <can.sardaslar@me.com>
2022-09-29 23:24:23 -0600

    push

--- server.js
+++ server.js
@@ -3,11 +3,44 @@ const { route } = require('./routes');
 const api = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
+let notes = require('./db/db.json');
+const { uid } = require('uid');
 
 app.use(express.static('public'));
 app.use(express.json());
 app.use('/api', api);
 
+
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+
+app.get('api/notes', (req, res) => {
+    res.json(notes)
+})
+
+
+
+app.post('/api/notes', (req, res) => {
+    let newNote = {
+        title: req.body.title,
+        text: req.body.text, 
+        id: uid()
+    }
+
+    notes.push(newNote)
+    res.json(200)
+})
+
+app.post('/api/notes', (req,res) => {
+    res.json(notes)
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+      notes=notes.filter(note => note.id | req.params.id)
+      res.json(notes)
+})
 
 app.listen(PORT, () => {
     console.log(`server start on: ${PORT}`);
